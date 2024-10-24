3d10cb87777d0b716b95c094de1c0c2df1724930
Edgar Mendez <edgarmendezcruz75@gmail.com>
2024-10-24 17:49:20 -0500

    add project

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+            fontFamily: {
+                jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+                poppins: ['Poppins', 'sans-serif'],
+            }
+        },
+        colors: {
+            header: "#222222",
+            primary: "#1c1817",
+            footer: "#191919",
+            light: "#FFF",
+            dark: "#000",
+            secondary: "#C5C5C5",
+            "secondary-txt": "#8491A0"
+        },
+        backgroundSize:{
+            "general-size": "656px"
+        },
+        fontSize: {
+            "2.5xl":"1.75rem"
+        }
+	},
+
+	plugins: [],
+
+}
